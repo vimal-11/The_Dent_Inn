879e54142fdb94593ea2745e5bce1b36fd19591b
vimal-11 <vimalaadith.bs@gmail.com>
2022-04-20 23:25:51 +0530

    first commit

---
+++ The_Dent_Inn/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for The_Dent_Inn project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'The_Dent_Inn.settings')
+
+application = get_wsgi_application()
