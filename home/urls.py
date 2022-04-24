353ac1de9819ebeae2b4d268344d868cf42cdd28
vimal-11 <vimalaadith.bs@gmail.com>
2022-04-24 23:46:23 +0530

    added webpages

--- home/urls.py
+++ home/urls.py
@@ -5,4 +5,8 @@ app_name = 'home'
 urlpatterns = [
     path('', views.home, name='home'),
     path('contact/', views.contact, name='contact'),
+    path('about/', views.about, name='about'),
+    path('pricing/', views.pricing, name='pricing'),
+    path('service/', views.service, name='service'),
+    path('appointment/', views.appointment, name='appointment'),
 ]
--- home/views.py
+++ home/views.py
@@ -28,3 +28,35 @@ def contact(request):
 
     else: 
         return render(request, 'contact.html', {})
+
+def about(request):
+    return render(request, 'about.html', {})
+
+def service(request):
+    return render(request, 'service.html', {})
+
+def pricing(request):
+    return render(request, 'price.html', {})
+
+def appointment(request):
+    if request.method == 'POST':
+        treatment = request.POST['treatment']
+        contact_no = request.POST['contact-no']
+        patient_name = request.POST['patient-name']
+        patient_email = request.POST['patient-email']
+        date = request.POST['slot-date']
+        slot_time = request.POST['slot-time']
+
+        # send mail
+
+        # send_mail(
+        #     message_sub,
+        #     message,
+        #     message_email,
+        #     [settings.EMAIL_HOST_USER],
+        # )
+
+        return render(request, 'appointment.html', {'patient_name': patient_name})
+
+    else: 
+        return render(request, 'appointment.html', {})
