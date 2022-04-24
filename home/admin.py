a4d7eb177d4189bc13c23ae643eccc1048ea4ab7
vimal-11 <vimalaadith.bs@gmail.com>
2022-04-25 00:25:11 +0530

    models for appointments

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from home.models import Appointments
 
 # Register your models here.
+
+class AppointmentsAdmin(admin.ModelAdmin):
+    list_filter = ['patient_name']
+    list_display = ['patient_name', 'email', 'contact_no',]
+    search_fields = ['patient_name', 'email', 'contact_no', 'date', 'treatment']
+    
+    class Meta:
+        model = Appointments
+
+admin.site.register(Appointments, AppointmentsAdmin)
--- home/models.py
+++ home/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
 
 # Create your models here.
+
+class Appointments(models.Model):
+    patient_name = models.CharField(max_length=100)
+    email = models.EmailField(verbose_name="email", max_length=60, unique=True)
+    contact_no = PhoneNumberField(null=False, blank=False, unique=True)
+    treatment = models.CharField(max_length=100) #must be changes to options
+    date = models.DateField(null=True)
+    time_slot = models.TimeField(null=True)
+    is_active = models.BooleanField(default=False)
+    is_done = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.patient_name
